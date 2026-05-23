81b6ec77448bc5f89ed909366c0bb77928f524d8
mayumar <maytalbada@gmail.com>
2026-05-23 12:36:36 +0200

    Actualizando rutas en config.yaml y configurando variables de entorno en main.py

--- prueba/main.py
+++ prueba/main.py
@@ -1,3 +1,8 @@
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 import argparse
 import os
 import pandas as pd
